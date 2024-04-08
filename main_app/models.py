e443f9272ced25e28c5104233924c96b1f404ba6
Ton Ha <ton@tdha.co>
2024-04-08 15:28:12 +1000

    Added model.

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Cat(models.Model):
+    name = models.CharField(max_length=128)
+    breed = models.CharField(max_length=128)
+    description = models.TextField(max_length=256)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}, {self.age}" # returns a string
---
+++ main_app/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # path(route, action_function, name='optional')
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cats_index, name='index'),
+]

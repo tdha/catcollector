a4b95a95e0961804603f6a4ce6a4179c7b8bab0b
Ton Ha <ton@tdha.co>
2024-04-12 12:00:35 +1000

    Added sign up

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,8 @@ import boto3
 from django.shortcuts import render, redirect
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 from .models import Cat, Toy, Photo
 
@@ -102,3 +104,21 @@ def add_photo(request, cat_id):
             print('An error occurred uploading file to S3')
             print(e)
     return redirect('detail', cat_id=cat_id)
+
+# SIGNUP ##################################################
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':   
+        form = UserCreationForm(request.POST) # this is how we create a 'user' form object that includes the data from the browser
+        if form.is_valid():
+            user = form.save() # this will add the user to the database
+            login(request, user) # automatically log in the new user
+            return redirect('index')
+        else:
+            error_message = 'Invalid sign up. Try again.'
+
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
